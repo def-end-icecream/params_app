47553ac08be391e7fa55fb749f1743f0c21dcd33
dani <dani.zaghian@gmail.com>
2021-01-14 12:03:41 -0800

    adds query params actions

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -8,4 +8,27 @@ class Api::ParamsExamplesController < ApplicationController
     render "query_params.json.jb"
   end
 
+  def name_action
+    # params = {"my_name"=>"dani"}
+    up_name = params["my_name"].upcase
+    @output_message = "Your name is #{up_name}"
+    if up_name.starts_with?("A")
+      @output_message = "Hey, your name starts with the first letter of the alphabet!"
+    end
+    render "name.json.jb"
+  end
+
+  def guessing_game_action
+    winning_number = 36
+    user_guess = params[:guess].to_i
+    if user_guess < winning_number
+      @output_message = "Your guess is too low!"
+    elsif user_guess > winning_number
+      @output_message = "Your guess is too high!"
+    else
+      @output_message = "You WIN!"
+    end
+    render "guessing_game.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/query_params_path" => "params_examples#query_params_action"
+    get "/name_path" => "params_examples#name_action"
+    get "/guessing_game_path" => "params_examples#guessing_game_action"
   end
 end
